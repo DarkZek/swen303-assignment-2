ff1fb21c7e8c5e1f5b3795bec0a45d281980208a
Marshall Scott <mashdowne@hotmail.co.nz>
2023-05-14 18:06:54 +1200

    Rebecca's changes

--- src/pages/FavouriteProducts.js
+++ src/pages/FavouriteProducts.js
@@ -1,4 +1,4 @@
-import { IonBadge, IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInfiniteScroll, IonInfiniteScrollContent, IonNote, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
+import { IonBadge, IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonCardSubtitle, IonIcon, IonInfiniteScroll, IonInfiniteScrollContent, IonNote, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
 import { cart, chevronBackOutline } from "ionicons/icons";
 import { useEffect, useRef, useState } from "react";
 import ProductCard from "../components/ProductCard";
@@ -7,15 +7,20 @@ import { FavouritesStore } from "../data/FavouritesStore";
 import { ProductStore } from "../data/ProductStore";
 
 import styles from "./CategoryProducts.module.css";
-
+const favouritesID = []
 const FavouriteProducts = () => {
 
     const cartRef = useRef();
+    const [category, setCategory] = useState({})
     const products = ProductStore.useState(s => s.products);
     const favourites = FavouritesStore.useState(s => s.product_ids);
     const shopCart = CartStore.useState(s => s.product_ids);
+    const [product, setProduct] = useState({});
     const [ searchResults, setSearchResults ] = useState([]);
-    const [ amountLoaded, setAmountLoaded ] = useState(6);
+    const [amountLoaded, setAmountLoaded] = useState(6);
+    
+    var maxProd = 4
+
 
     useEffect(() => {
 
@@ -28,7 +33,9 @@ const FavouriteProducts = () => {
                 var favouriteParts = favourite.split("/");
                 var categorySlug = favouriteParts[0];
                 var productID = favouriteParts[1];
-                
+
+                favouritesID.push(parseInt(productID))
+
                 const tempCategory = products.filter(p => p.slug === categorySlug)[0];
                 const tempProduct = tempCategory.products.filter(p => parseInt(p.id) === parseInt(productID))[0];
 
@@ -37,7 +44,8 @@ const FavouriteProducts = () => {
                     category: tempCategory,
                     product: tempProduct
                 };
-
+                setCategory(tempCategory);
+                setProduct(tempProduct);
                 setSearchResults(prevSearchResults => [ ...prevSearchResults, tempFavourite ]);
             });
         }
@@ -91,6 +99,30 @@ const FavouriteProducts = () => {
                                 return (
                                     <ProductCard key={ `category_product_${ index }`} product={ product.product } index={ index } cartRef={ cartRef } category={ product.category } />
                                 );
+                            }
+                        })}
+                    </IonRow>
+
+                    <IonRow className="ion-text-center">
+                        <IonCol size="12">
+                            <IonCardSubtitle>Today's popular products...</IonCardSubtitle>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        {(category && category.products) && category.products.map((similar, index) => {
+                            if (favouritesID.includes(similar.id)) {
+                                maxProd++;
+
+                            }
+
+                           
+                            
+                            if (!favouritesID.includes(similar.id) && (product.image && index < maxProd)) {
+                                                      return (
+
+                                        <ProductCard key={`similar_product_${index}`} product={similar} index={index} isFavourite={false} cartRef={cartRef} category={category} />
+                                    );
+                                
                             }
                         })}
                     </IonRow>
